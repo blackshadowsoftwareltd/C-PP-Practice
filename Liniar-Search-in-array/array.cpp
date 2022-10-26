0044d4bb6471ac4f2aede865f18f6698bf39ea10
remon <blackshadowsoftwareltd@gmail.com>
2022-10-26 22:04:42 +0600

    Linear Search

---
+++ Liniar-Search-in-array/array.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+  int main()
+{
+    int size, i, item, position = 0;
+    cout << "Enter the size of an Array : ";
+
+    cin >> size;
+    int arr[size];
+
+    cout << "Enter the elements of an Array : ";
+    for (i = 0; i < size; i++)
+        cin >> arr[i];
+
+      cout << "Enserted elements of Array is : ";
+    for (i = 0; i < size; i++)
+        cout << arr[i] << " ";
+
+    cout << "\nEnserted the search Item : ";
+    cin >> item;
+
+      while (arr[position] != item && position < size)
+        position++;
+      if (position == size)
+            cout
+        << "Item does not exis in this Array";
+    else cout << position << " is the location of this item " << item;
+
+      cout << endl;
+}
